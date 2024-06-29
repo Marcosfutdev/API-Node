aef131d9776fed6a11e0658c16dfb1ac31cad713
Marcosfutdev <madss0102@gmail.com>
2024-06-29 19:48:44 -0300

    first commit

---
+++ express.js
@@ -0,0 +1,64 @@
+const express = require('express');
+// model
+const UserModel = require('./src/models/use.model');
+const app = express();
+app.use(express.json());
+
+app.get('/users', async (req, res) => {
+    try{
+       const users = await UserModel.find({});
+
+        return res.status(200).json(users);
+    }
+    catch (error) {
+        return res.status(500).send(error.message);
+    }
+});
+
+app.get('/users/:id', async (req, res) => {
+   try{
+    const id = req.params.id;
+    const user = await UserModel.findById(id);
+    return res.status(200).json(user);
+   }
+   catch(error){
+        return res.status(500).send(error.message);
+   }
+});
+
+app.post('/users', async (req, res) => {
+   try{
+    const user = await UserModel.create(req.body);
+
+    res.status(201).json(user);
+   }
+   catch (error){
+    return res.status(500).send(error.message);
+   }  
+})
+
+app.patch('/users/:id', async (req, res) => {
+    try{
+        const id = req.params.id;;
+        const user = await UserModel.findByIdAndUpdate(id, req.body, {new:true});
+        res.status(200).json(user);
+    }
+    catch(error){
+        return res.status(500).send(error.message);
+    }
+});
+
+app.delete('/users/:id', async (req, res) => {
+    try{
+        const id = req.params.id;
+        const user = await UserModel.findOneAndDe(id);
+        res.status(200).json(user);
+    }
+    catch(error){
+       res.status(500).send(error.message)
+    }
+});
+
+const port = 2020;
+
+app.listen(port, () => {console.log(`Servidor rodando com sucesso na porta: ${port}`)});
